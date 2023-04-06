d4b206be489dff5f6ac97ebf600c058181396f0a
Vruddhi-shah <vruddhishah1997@gmail.com>
2023-04-06 12:54:08 +0530

    Added unit testing for the methods

--- CaseStudy_TCS/Controllers/EmployeeController.cs
+++ CaseStudy_TCS/Controllers/EmployeeController.cs
@@ -13,6 +13,15 @@ namespace CaseStudy_TCS.Controllers
     {
         public static readonly HttpClient client = new HttpClient();
 
+        List<EmployeeDetails> employeeDetails1 = new List<EmployeeDetails>();
+
+        public EmployeeController() { }
+
+        public EmployeeController(List<EmployeeDetails> employeeDetails)
+        {
+            this.employeeDetails1 = employeeDetails;
+        }
+
         public ActionResult GetEmployee(int page = 0)
         {
             return View();
---
+++ CaseStudy_TCS_UnitTest/UnitTest1.cs
@@ -0,0 +1,49 @@
+using CaseStudy_TCS.Controllers;
+using CaseStudy_TCS.Models;
+
+namespace CaseStudy_TCS_UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void GetEmployee()
+        {
+            var testEmployeeDetails = GetTestEmployee();
+            var controller = new EmployeeController(testEmployeeDetails);
+            var result = controller.GetEmployeeList("1",1,4,null) ;
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void GetEmployeeDetailById()
+        {
+            var employeeId = 1;
+            var testEmployeeDetails = GetTestEmployee();
+            var controller = new EmployeeController(testEmployeeDetails);
+            var result = controller.EmployeeDetails(employeeId);
+            Assert.IsNotNull(result);
+        }
+
+        private List<EmployeeDetails> GetTestEmployee()
+        {
+            var testEmployeeDetails = new List<EmployeeDetails>();
+            testEmployeeDetails.Add(new EmployeeDetails { id = 1, Name = "Test1", Email ="Test1@gmail.com" ,Gender = "Male",Status = "Active" });
+            testEmployeeDetails.Add(new EmployeeDetails { id = 2, Name = "Test2", Email = "Test2@gmail.com", Gender = "FeMale", Status = "InActive" });
+            testEmployeeDetails.Add(new EmployeeDetails { id = 3, Name = "Test3", Email = "Test3@gmail.com", Gender = "Male", Status = "Active" });
+            testEmployeeDetails.Add(new EmployeeDetails { id = 4, Name = "Test4", Email = "Test4@gmail.com", Gender = "FeMale", Status = "InActive" });
+            testEmployeeDetails.Add(new EmployeeDetails { id = 5, Name = "Test5", Email = "Test5@gmail.com", Gender = "FeMale", Status = "InActive" });
+            return testEmployeeDetails;
+        }
+    }
+}
